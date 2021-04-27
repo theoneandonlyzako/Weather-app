e6c96b5c13654df626a52e1b2fb661bdac902750
Zak Stamps <theoneandonlyzako@gmail.com>
2021-04-27 16:42:00 -0700

    added dynamic elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,57 @@
-displayWeather() {
-    iconElement.innerHTML = 
-    tempElement.innerHTML = 
-    descElement.innerHTML = 
-    locationElement.innerHTML = 
-};
-
-weather.tempurature.value = 300 -275
-
-function celsiusToFahrenheit() {
-    (tempurature * 9/5) + 32;
-}
-
-// performs the search when button is clicked
-$("#save-btn").on("click", function(event){
-    event.preventDefault();
-
-    cityName = $("#cityInput").val().trim();
-    if(cityname === "") {
-        alert("Please Enter a Valid City Name")
-    } else if (cityList.length >= 5) {
-        cityList.shift();
-        cityList.push(cityname);
-    }else {
-        cityList.push(cityname);
-    }
-    storeCurrentCity()
-    saveCityArray()
+// let weather = {
+  $(window).on("load", function () {
+  console.log("apikey");
+  
+  var apiKey = "ede1a7baf3cbf299883575c9bf004bbc"
+  function fetchWeather(city) {
+   return fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => displayWeather(data));
+  }
+  function displayWeather(data) {
+    console.log("displllll");
     
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    
+    for(let i=0; i < 6; i++) {
+      var container = $('.weather-container')
+      var parentDiv = $('<div>')
+      var cityEl = $('<h2 class"city"></h2>')
+      var iconDiv = $('<div id="icon">')
+      var imgEl = $('<img src="https://img.icons8.com/officel/30/000000/sun.png">')
+      var tempDiv = $('<div class="temp">')
+      var humidityEl = $('<h3>Humidity</h3>')
+      var windEl = $('<h3>Wind</h3>')
+      var uvEl = $('<h3>UV</h3>')
+      
+      parentDiv .append(cityEl)
+      iconDiv .append(imgEl)
+      parentDiv .append(iconDiv)
+      tempDiv .append(humidityEl)
+      tempDiv .append(windEl)
+      tempDiv .append(uvEl)
+      parentDiv .append(tempDiv)
+
+      container .append(parentDiv)
+      // console.log(imgIcon);
+      
+    }
+    document.querySelector(".city").innerText = "weather in" + name;
+
+    $(".icon").attr("src","http://openweathermap.org/img/wn/" + icon + ".png");
+  
+    // document.querySelector(".description").innerText = "weather in" + name;
+    // document.querySelector(".temp").innerText = "weather in" + name;
+    // document.querySelector(".humidity").innerText = "weather in" + name;
+    // document.querySelector(".speed").innerText = "weather in" + name;
+  }
+
+  fetchWeather("benicia")
 })
 
-function storeCurrentCity() {
-localStorage.setItem("CurrentCity", JSON.stringify(cityname));
-console.log(cityname);
-
-}
-
-function saveCityArray() {
-
-}

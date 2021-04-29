9213168fdfafd084011acd5b9ecaac6686a98586
Zak Stamps <theoneandonlyzako@gmail.com>
2021-04-28 17:45:07 -0700

    added iput field value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // let weather = {
   $(window).on("load", function () {
-  console.log("apikey");
+  // console.log("apikey");
   
   var apiKey = "ede1a7baf3cbf299883575c9bf004bbc"
   function fetchWeather(city) {
    return fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey
+      "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey
     )
       .then((response) => response.json())
       .then((data) => displayWeather(data));
@@ -18,7 +18,31 @@
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     console.log(name, icon, description, temp, humidity, speed);
-    
+
+    // Dynamically builds the current temp area
+    var container = $('.current')
+    var parentDiv = $('<div class="day">')
+    var cityEl = $('<h2 class="city"></h2>')
+    var iconDiv = $('<div id="icon">')
+    var tempDiv = $('<div class="temp"></div>')
+    var imgEl = $('<img class="icon">')
+    var barDiv = $('<div class="bar">')
+    var humidityEl = $('<h3 class="humidity"></h3>')
+    var windEl = $('<h3 class="wind"></h3>')
+    var uvEl = $('<h3 class="uv"></h3>')
+      
+    parentDiv .append(cityEl)
+    iconDiv .append(imgEl)      
+    parentDiv .append(iconDiv)
+    parentDiv .append(tempDiv)
+    barDiv .append(humidityEl)
+    barDiv .append(windEl)
+    barDiv .append(uvEl)
+    parentDiv .append(barDiv)
+
+    container .append(parentDiv)
+
+    // Dynamically builds the 5 day forcast
     for(let i=0; i < 5; i++) {
       var container = $('.weather-container')
       var parentDiv = $('<div class="day">')
@@ -45,10 +69,11 @@
       // console.log(imgIcon);
 
       $(".city").text(name);
+      $(".temp").text(temp+"°");
       $(".icon").attr("src","http://openweathermap.org/img/wn/" + icon + ".png");
-      $(".humidity").text(name);
-      $(".wind").text(name);
-      $(".uv").text(name);
+      $(".humidity").text("humidity "+humidity);
+      $(".wind").text("wind "+speed);
+      // $(".uv").text(uv);
     } 
 
     // for(let i=0; i < 6; i++) {
@@ -61,6 +86,12 @@
     // document.querySelector(".speed").innerText = "weather in" + name;
   }
 
-  fetchWeather("benicia")
+  // fetches the weather when btn is clicked
+  $("#save-btn").on("click", function() {
+    var searchResult = $("#cityInput").val()
+    fetchWeather(searchResult)
+    console.log(searchResult);
+  })
+  // fetchWeather("benicia")
 })
 
